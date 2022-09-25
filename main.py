a692aaf31ca6e8e361ab51748e603133e284996b
powerlock <michael.guo002@gmail.com>
2022-09-25 08:30:17 -0700

    comment out model main line

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ def get_prediction(payload: StockIn):
         "forecast": convert(prediction_list)}
     return response_object
 
-@app.get("/health", response_model=StockOut, status_code=200)
+#@app.get("/health", response_model=StockOut, status_code=200)
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
