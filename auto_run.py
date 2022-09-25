60da1f291e7ef69d6b49f21287c0cf4bbc4e5525
powerlock <michael.guo002@gmail.com>
2022-09-25 00:49:11 -0700

    update file path

--- auto_run.py
+++ auto_run.py
@@ -19,7 +19,7 @@ def auto(stlist):
         train(args.ticker)
         #time.sleep(5)
 file = 'nasdaq_screener_1664091848249.csv'
-df = pd.read_csv(str(BASE_DIR)+'/data'+'/file')
+df = pd.read_csv(str(BASE_DIR)+'/data'+'/'+file)
 stlist = df['Symbol'].iloc[0:100]
 auto(stlist)
 #schedule model update everyday midnight
