b4c42120b4c679d53a3f86e979522925e1da7489
powerlock <michael.guo002@gmail.com>
2022-09-25 21:55:28 -0700

    add execution prediction file

--- predict.py
+++ predict.py
@@ -8,19 +8,22 @@ def pred(yesterday, stlist):
         invest = []
         for s,t in zip(yesterday,stlist):
             print("Analyzing stock ", t)
-            parser = argparse.ArgumentParser(description='predict')
-            parser.add_argument('--ticker', type=str, default=t, help='Stock Ticker')
-            parser.add_argument('--days', type=int, default=7, help='Number of days to predict')
-            args = parser.parse_args()
-            prediction_list = predict(ticker=args.ticker, days=args.days)
-            output = convert(prediction_list)
-            
-            val = (s - next(iter(output.values())))/s
-            if abs(val) > 0.15:
-                invest.append(s)
-                print("****************15 percent more change, High chance to invest*************")
-                print(s, t, val, output)
-                print("                                                    ")
+            if t in['RTX', 'LOW']:
+                continue
+            else:
+                parser = argparse.ArgumentParser(description='predict')
+                parser.add_argument('--ticker', type=str, default=t, help='Stock Ticker')
+                parser.add_argument('--days', type=int, default=7, help='Number of days to predict')
+                args = parser.parse_args()
+                prediction_list = predict(ticker=args.ticker, days=args.days)
+                output = convert(prediction_list)
+                
+                val = (s - next(iter(output.values())))/s
+                if abs(val) > 0.15:
+                    invest.append(s)
+                    print("****************15 percent more change, High chance to invest*************")
+                    print(s, t, val, output)
+                    print("                                                    ")
         print("Invest list is  ",invest)
 print("prediction starts..")
 file = 'nasdaq_screener_1664091848249.csv'
